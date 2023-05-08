7d23b4655b0a6c509b82a8be66cb57bcae7374ab
Björnen <112392165+purpledusty@users.noreply.github.com>
2023-05-08 14:20:45 +0200

    Delete js directory

--- js/overlayscreen.js
+++
@@ -1,9 +0,0 @@
-export class OverlayScreen {
-
-	overlayScreen = document.querySelector('#overlayScreen')
-
-	removeOverlayScreen() {
-		// Remove everything below overlayScreen tag 
-		overlayScreen.innerHTML = ''
-	}
-}
